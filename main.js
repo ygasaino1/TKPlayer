cb4c3cb4f225c23f1092b40b1090a14e5afe1627
Tiki <54782797+ygasaino1@users.noreply.github.com>
2022-05-29 17:44:07 +0430

    ....

--- main.js
+++ main.js
@@ -47,12 +47,13 @@ function cmd_open() {
             link = homepage;
             link_url = new URL(link);
             console_(`URL ${hash['value']}`);
+            prehub();
         } else {
             link_url = new URL(link);
             log = `URL [${link_url.protocol}//][${link_url.host}][${link_url.pathname}][${link_url.search}][${link_url.hash}] -${JSON.stringify(parameters)}`;
             console_(log);
+            prehub();
         }
-        prehub();
     } catch (e) {
         console.log('URL Failed');
         if (link_url == null) { cmd_debug(`URL ${hash['value']}`), debug_cl_warning };
