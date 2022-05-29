392e506ffbe2a696697caefe3c978cc2a10834cc
Tiki <54782797+ygasaino1@users.noreply.github.com>
2022-05-29 21:16:00 +0430

    ....

--- main.js
+++ main.js
@@ -153,11 +153,11 @@ function cmd_debug(msg, color = debug_cl_warning) {
 // log = `REQUEST/Video<br>URL [${url.protocol || ''}//][${url.host || ''}][${url.pathname || ''}][${url.search || ''}][${url.hash || ''}]`;
 function console_(log, color = debug_cl_default) {
     if (log != '') {
-        try {
-            log = decodeURI(log);
-            log = msg.replaceAll('%20', ' ');
-            log = msg.replaceAll('\\n', '<br>');
-        } catch {}
+        // try {
+        //     log = decodeURI(log);
+        // } catch { }
+        log = msg.replaceAll('%20', ' ');
+        log = msg.replaceAll('\\n', '<br>');
     };
     let this_ = htmlToElement(`<div class="console" style="color: ${color}">${log}</div>`);
     console_input.appendChild(this_);
